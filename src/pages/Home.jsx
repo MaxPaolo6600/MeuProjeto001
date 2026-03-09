af3f0a6996fa37e7e260a79c72819d7a13170ea3
MaxPaolo660 <paolomax993@gmail.com>
2026-03-09 09:42:10 -0300

    cgn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header"
 
 export default function Home() {
+    const navigate = useNavigate();
     return (
         <>
             <Header />
-            <h1>oi</h1>
+            <main className="container mx-auto">
+                <div className="flex items-center flex-col">
+                    <h1>Essa é a página Home, crie sua conta</h1>
+                    <button className="text-white w-100 rounded-2xl bg-linear-to-bl from-violet-500 to-fuchsia-500" onClick={() => navigate("/Cadastrar")}>AQUI</button>
+                </div>
+            </main>
         </>
     )
 }
